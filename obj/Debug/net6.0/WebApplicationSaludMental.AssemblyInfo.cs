56bfada8e4b7dcbe76548a2e7e06fc4c7cd0b986
JairAlexey <alexeyrueda823@gmail.com>
2024-06-03 19:31:34 -0500

    Error tipeo

--- obj/Debug/net6.0/WebApplicationSaludMental.AssemblyInfo.cs
+++ obj/Debug/net6.0/WebApplicationSaludMental.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplicationSaludMental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71354baf60029b68e29771eb1ed97ff6c62b4cff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a97cdbd0b97852f0dae6d90511504e7531046f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplicationSaludMental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplicationSaludMental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
